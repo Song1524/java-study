38e596398554a8e0131f46fe2a968c126518b08b
Song1524 <zsdx1525@gmail.com>
2025-07-16 23:16:30 +0900

    ???

--- chap02/src/sec04/exam05/ScannerExample.java
+++ chap02/src/sec04/exam05/ScannerExample.java
@@ -8,17 +8,17 @@ public class ScannerExample {
 		Scanner sc = new Scanner(System.in);
 		String inputData;
 		
-		while (true) {
-			inputData = sc.nextLine(); // 스캐너에서 읽은 한 줄 전체(엔터키 이전까지)
-			System.out.println("입력된 문자열: " + inputData);
-			
-//			if (inputData == "q") { // (테스트)
-			if (inputData.equals("q")) {
-				break;
-			}
-		}
-		
-		System.out.println("종료\n");
+//		while (true) {
+//			inputData = sc.nextLine(); // 스캐너에서 읽은 한 줄 전체(엔터키 이전까지)
+//			System.out.println("입력된 문자열: " + inputData);
+//			
+////			if (inputData == "q") { // (테스트)
+//			if (inputData.equals("q")) {
+//				break;
+//			}
+//		}
+//		
+//		System.out.println("종료\n");
 		
 		// Quiz
 //		2개의 int 값을 입력받아 입력받은 값을 화면에 ,로 연결하여 출력하시오.
