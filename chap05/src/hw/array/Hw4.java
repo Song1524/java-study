da77f698a4701fda4d75b88a42bacf53891056a1
zsdx1524 <zsdx1525@gmail.com>
2025-07-25 10:17:29 +0900

    배열 과제 풀이

--- chap05/src/hw/array/Hw4.java
+++ chap05/src/hw/array/Hw4.java
@@ -31,3 +31,32 @@ public class Hw4 {
 	}
 
 }
+	// 선생님 풀이
+//int[] lottoArr = new int[6];
+//int count = 0;
+//
+//while (count < 6) {
+//	int num = (int) (Math.random() * 45) + 1;
+//	
+//	// 중복 체크
+//	boolean dup = false;
+//	for (int i = 0; i < count; i++) { // 실제 로또 번호가 들어간 count까지만 체크
+//		if (lottoArr[i] == num) {
+//			dup = true;
+//			break; // 중복되는 값을 찾은 즉시 빠져나감
+//		}
+//	}
+//	
+//	// 중복이 아니면 배열에 저장
+//	if (!dup) {
+//		lottoArr[count] = num;
+//		count++;
+//	}
+//}
+//
+//// 오름차순 정렬
+//Arrays.sort(lottoArr);
+//
+//for (int num : lottoArr) {
+//	System.out.print(num + " ");
+//}
